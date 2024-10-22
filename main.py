c6a50605ec13bbe9125ee67d3e2f37683d3486e2
Ivan Ho <73658947+iho21@users.noreply.github.com>
2024-10-22 07:19:57 +0000

    New changes

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import numpy as np
 import os
 from openai import OpenAI
 
-client = OpenAI(
-  base_url="https://api.groq.com/openai/v1",
-  api_key=os.environ.get("GROQ_API_KEY")
-)
+client = OpenAI(base_url="https://api.groq.com/openai/v1",
+                api_key=os.environ.get("GROQ_API_KEY"))
+
+
 def load_model(filename):
   with open(filename, "rb") as file:
     return pickle.load(file)
+
 
 xgboost_model = load_model('xgb_model.pkl')
 
@@ -31,11 +32,48 @@ xgboost_SMOTE_model = load_model('xgboost-SMOTE.pkl')
 
 xgboost_featureEngineered_model = load_model('xgboost-featureEngineered.pkl')
 
+
+def prepare_input(credit_score, location, gender, age, tenure, balance,
+                  num_products, has_credit_card, is_active_member,
+                  estimated_salary):
+  input_dict = {
+      'CreditScore': credit_score,
+      'Age': age,
+      'Tenure': balance,
+      'NumOfProducts': num_products,
+      'HasCrCard': int(has_credit_card),
+      'IsActiveMember': int(is_active_member),
+      'EstimatedSalary': estimated_salary,
+      'Geography_France': 1 if location == "France" else 0,
+      'Geography_Germany': 1 if location == "Germany" else 0,
+      'Geography_Spain': 1 if location == "Spain" else 0,
+      'Gender_Male': 1 if gender == 'Male' else 0,
+      'Gender_Female': 1 if gender == 'Female' else 0
+  }
+  input_df = pd.DataFrame([input_dict])
+  return input_df, input_dict
+
+
+def make_predictions(input_df, input_dict):
+  probabilities = {
+      'XGBoost': xgboost_model.predict_proba(input_df)[0][1],
+      'Random Forest': random_forest_model.predict_proba(input_df)[0][1],
+      'K-Nearest Neighbors': knn_model.predict_proba(input_df)[0][1],
+  }
+  avg_probability = np.mean(list(probabilities.values()))
+  st.markdown("### Model Probabilities")
+  for model, prob in probabilities.items():
+    st.write(f"{model} {prob}")
+  st.write(f"Average Probability: {avg_probability}")
+
+
 st.title("Customer Churn Prediction")
 
 df = pd.read_csv("churn.csv")
 
-customers = [f"{row['CustomerId']} - {row['Surname']}" for _, row in df.iterrows()]
+customers = [
+    f"{row['CustomerId']} - {row['Surname']}" for _, row in df.iterrows()
+]
 
 selected_customer_option = st.selectbox("Select a customer", customers)
 
@@ -51,49 +89,41 @@ if selected_customer_option:
 
   with col1:
 
-    credit_score = st.number_input(
-      "Credit Score",
-      min_value=300,
-      max_value=850,
-      value=int(selected_customer['CreditScore'])
-    )
-    location = st.selectbox(
-      "Location", ["Spain", "France", "Germany"],
-      index=["Spain", "France",     "Germany"].index(selected_customer['Geography']))
+    credit_score = st.number_input("Credit Score",
+    min_value=300,
+    max_value=850,
+    value=int(selected_customer['CreditScore']))
+    location = st.selectbox("Location", ["Spain", "France", "Germany"],
+    index=["Spain", "France", "Germany"].index(selected_customer['Geography']))
+    gender = st.radio("Gender", ["Male", "Female"],
+    index = 0 if selected_customer['Gender'] == 'Male' else 1)
+    age = st.number_input("Age",
+                          min_value=18,
+                          max_value=100,
+             value=int(selected_customer['Age']))
 
-    gender = st.radio("Gender", ["Male", "Female"], index=0 if     selected_customer['Gender'] == 'Male' else 1)
-
-    age = st.number_input(
-      "Age",
-      min_value=18,
-      max_value=100,
-      value=int(selected_customer['Age']))
-
-    tenure = st.number_input(
-      "Tenure (years)",
-      min_value=0,
-      max_value=50,
-      value=int(selected_customer['Tenure']))
+    tenure = st.number_input("Tenure (years)",
+                             min_value=0,
+                             max_value=50,
+                             value=int(selected_customer['Tenure']))
   with col2:
-      balance = st.number_input(
-        "Balance",
-        min_value=0.0,
-        value=float(selected_customer['Balance']))
-      num_products = st.number_input(
-        "Number pf Products",
-        min_value=1,
-        max_value=10,
-        value=int(selected_customer['IsActiveMember']))
-      is_active_member = st.checkbox(
-        "Is Active Member",
-        value=bool(selected_customer['IsActiveMember'])
-      )
-      estimated_salary = st.number_input(
+    balance = st.number_input("Balance",
+                              min_value=0.0,
+                              value=float(selected_customer['Balance']))
+    num_products = st.number_input("Number of Products",
+                                   min_value=1,
+                                   max_value=10,
+                                   value=int(
+                                       selected_customer['IsActiveMember']))
+    has_credit_card = st.checkbox("Has Credit Card", value=bool(selected_customer['HasCrCard']))
+    is_active_member = st.checkbox("Is Active Member",
+                                   value=bool(
+                                       selected_customer['IsActiveMember']))
+    estimated_salary = st.number_input(
         "Estimated Salary",
         min_value=0.0,
-        value=float(selected_customer['EstimatedSalary'])
-      )
-      
+    value=float(selected_customer['EstimatedSalary']))
 
+  input_df, input_dict = prepare_input(credit_score, location, gender, age, tenure, balance, num_products, has_credit_card, is_active_member, estimated_salary)
 
-  
+  make_predictions(input_df, input_dict)
