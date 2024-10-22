0f5e272bac107fa06abfb95467884e6d6818a096
hoivan08 <34490860-hoivan08@users.noreply.replit.com>
2024-10-22 06:15:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import numpy as np
+import os
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://api.groq.com/openai/v1",
+  api_key=os.environ.get("GROQ_API_KEY")
+)
+def load_model(filename):
+  with open(filename, "rb") as file:
+    return pickle.load(file)
+
+xgboost_model = load_model('xgb_model.pkl')
+
+naive_bayes_model = load_model('nb_model.pkl')
+
+random_forest_model = load_model('rf_model.pkl')
+
+decision_tree_model = load_model('dt_model.pkl')
+
+svm_model = load_model('svm_model.pkl')
+
+knn_model = load_model('knn_model.pkl')
+
+voting_classifier_model = load_model('voting_clf.pkl')
+
+xgboost_SMOTE_model = load_model('xgboost-SMOTE.pkl')
+
+xgboost_featureEngineered_model = load_model('xgboost-featureEngineered.pkl')
+
+st.title("Customer Churn Prediction")
+
+df = pd.read_csv("churn.csv")
+
+customers = [f"{row['CustomerId']} - {row['Surname']}" for _, row in df.iterrows()]
+
+selected_customer_option = st.selectbox("Select a customer", customers)
+
+if selected_customer_option:
+  selected_customer_id = int(selected_customer_option.split(" - ")[0])
+  print("Selected Customer ID", selected_customer_id)
+  selected_surname = selected_customer_option.split(" - ")[1]
+  print("Surname", selected_surname)
+  selected_customer = df.loc[df['CustomerId'] == selected_customer_id].iloc[0]
+
+  print("Selected Customer", selected_customer)
+  col1, col2 = st.columns(2)
+
+  with col1:
+
+    credit_score = st.number_input(
+      "Credit Score",
+      min_value=300,
+      max_value=850,
+      value=int(selected_customer['CreditScore'])
+    )
+    location = st.selectbox(
+      "Location", ["Spain", "France", "Germany"],
+      index=["Spain", "France",     "Germany"].index(selected_customer['Geography']))
+
+    gender = st.radio("Gender", ["Male", "Female"], index=0 if     selected_customer['Gender'] == 'Male' else 1)
+
+    age = st.number_input(
+      "Age",
+      min_value=18,
+      max_value=100,
+      value=int(selected_customer['Age']))
+
+    tenure = st.number_input(
+      "Tenure (years)",
+      min_value=0,
+      max_value=50,
+      value=int(selected_customer['Tenure']))
+  with col2:
+      balance = st.number_input(
+        "Balance",
+        min_value=0.0,
+        value=float(selected_customer['Balance']))
+      num_products = st.number_input(
+        "Number pf Products",
+        min_value=1,
+        max_value=10,
+        value=int(selected_customer['IsActiveMember']))
+      is_active_member = st.checkbox(
+        "Is Active Member",
+        value=bool(selected_customer['IsActiveMember'])
+      )
+      estimated_salary = st.number_input(
+        "Estimated Salary",
+        min_value=0.0,
+        value=float(selected_customer['EstimatedSalary'])
+      )
+      
+
+
+  
